712375f5b9ec65965920cf395641c5b7de3e97ed
Mayara-Souza <mayara.souza1523@gmail.com>
2024-02-13 23:10:36 -0300

    project: automating card interface creation via script

--- script/script.js
+++ script/script.js
@@ -1,1 +1,26 @@
-let cards
+let cards = ["Bioquímica", "Fisiopatologia", "Fisologia aplicada a psicobiologia"];
+let text_url = "https://share.note.sx/0q2sawx5#WqcvqOCyZb3Dusx/QTPEahNBC97SK4Jh8crx2XJyr20";
+let container = document.querySelector('#boundaries');
+
+function createCards(){
+    cards.forEach(card => {
+        let div = document.createElement('div');
+        div.className = 'card';
+
+        let a = document.createElement('a');
+        a.setAttribute('href', text_url);
+        
+        let span = document.createElement('span'); 
+        span.textContent = card;
+        
+        a.appendChild(span);
+        div.appendChild(a);
+        container.appendChild(div);
+    });
+}
+
+createCards();
+
+
+
+
