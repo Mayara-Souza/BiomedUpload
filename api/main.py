80c3c9cf282419a206f0bd85b12f559c3817394e
Mayara-Souza <90940393+Mayara-Souza@users.noreply.github.com>
2024-02-16 16:19:28 -0300

    files: tidying up directories

--- api/main.py
+++
@@ -1,9 +0,0 @@
-'''import json'''
-
-'''Escrever os dados no arquivo JSON json.load(file)'''
-
-with open('../data/materias.json', 'r') as arquivo:
-    conteudo = arquivo.read()
-    print(conteudo)
-
-'''../data/materias.json'''
